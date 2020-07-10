42ba084ffc30ef2c6402ddcbf0ae1becfdb62efb
galbertomeli <gustavo.alberto@mercadolibre.com>
2020-07-10 17:22:56 -0300

    fixed PlotPitchEvents qualified and plotPassMatrix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="This package allows to interact with opta files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/galberto/optareader",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
